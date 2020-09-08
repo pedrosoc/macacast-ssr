cd5c866a1a8cdbf4d1ee86f3a4d795129bf12632
Lucas Pedroso <lucas.pedroso@dextra-sw.com>
2020-09-08 13:31:21 -0300

    feat: fix social image

--- src/constants/images.js
+++ src/constants/images.js
@@ -3,7 +3,7 @@ import urls from "./urls";
 const images = {
     favicon: `${urls.gallery.baseUrl}/website_midia_favicon.png?alt=media`,
     logo: `${urls.gallery.baseUrl}/website_midia_macaco.png?alt=media`,
-    social: `${urls.gallery.baseUrl}/website_midia_social.png?alt=media`
+    social: `${urls.gallery.baseUrl}/website_midia_social.jpg?alt=media`
 };
 
 export default images;
