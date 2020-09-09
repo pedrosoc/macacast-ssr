465659b604467b6beee29428b918748a9d92e285
Lucas Pedroso <lucas.pedroso@dextra-sw.com>
2020-09-09 10:33:56 -0300

    feat: fix sitemap

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,6 +1,6 @@
 const urls = {
     website: {
-        baseUrl: "https://macacast-ssr.vercel.app"
+        baseUrl: "https://macacast.com.br"
     },
     gallery: {
         baseUrl: "https://firebasestorage.googleapis.com/v0/b/macacast-podcast.appspot.com/o"
