dee4b464d8ef2faaf3493a8ee8c8c9ac64b9bf56
Lucas Pedroso <lucas.pedroso@dextra-sw.com>
2021-03-06 00:08:54 -0300

    feat: new logo design

--- src/constants/images.js
+++ src/constants/images.js
@@ -3,9 +3,10 @@ import urls from "./urls";
 const images = {
     favicon: `/static/images/favicon.png`,
     logo: `${urls.gallery.baseUrl}/website_midia_macaco.png?alt=media`,
+    social: `${urls.gallery.baseUrl}/website_midia_social.png?alt=media`,
+    defaultCard: `${urls.gallery.baseUrl}/website_default_post_bg.png?alt=media`,
+    
     newLogo: `${urls.gallery.baseUrl}/website_midia_logo.png?alt=media`,
-    social: `${urls.gallery.baseUrl}/website_midia_social.jpg?alt=media`,
-    defaultCard: `${urls.gallery.baseUrl}/website_default_post_bg.jpeg?alt=media`,
     background: `${urls.gallery.baseUrl}/background.jpg?alt=media`
 };
 
