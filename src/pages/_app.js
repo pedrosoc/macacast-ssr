58aacab608231cf0fbb014fe55b55636397855e8
Lucas Pedroso <lucas.pedroso@dextra-sw.com>
2021-03-08 21:04:40 -0300

    feat: ga

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,41 @@
-import App from "next/app";
+import { Fragment, useEffect } from "react";
 
-import { Fragment } from "react";
+import App from "next/app";
+import { useRouter } from 'next/router'
 
 import GlobalStyle from "@global-style";
 import { appWithTranslation } from "@i18n";
+import { initGA, logPageView } from "@/utils/analytics";
 
-const MyApp = ({ Component, pageProps }) => (
-    <Fragment>
-        <GlobalStyle />
-        <Component {...pageProps} />
-    </Fragment>
-);
+const MyApp = ({ Component, pageProps }) => {
+    const router = useRouter()
+
+    useEffect(() => {
+        initGA()
+        // `routeChangeComplete` won't run for the first page load unless the query string is
+        // hydrated later on, so here we log a page view if this is the first render and
+        // there's no query string
+        if (!router.asPath.includes('?')) {
+            logPageView()
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    useEffect(() => {
+        // Listen for page changes after a navigation or when the query changes
+        router.events.on('routeChangeComplete', logPageView)
+        return () => {
+            router.events.off('routeChangeComplete', logPageView)
+        }
+    }, [router.events])
+
+    return (
+        <Fragment>
+            <GlobalStyle />
+            <Component {...pageProps} />
+        </Fragment>
+    );
+};
 
 MyApp.getInitialProps = async (appContext) => ({ ...await App.getInitialProps(appContext) })
 
